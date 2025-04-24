49fc7c2915b81d7b260f5ed6d9d365b7d90232eb
Kibeyy <collinskibe6477@gmail.com>
2025-04-24 23:55:21 +0300

    Dynamic image sizer and clickable app

---
+++ projects/gesture_detector_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double sliderValue = 50.0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("GESTURE DETECTIONS")
+          ,
+        ),
+        body: GestureDetector(
+          onTap: () {
+            print("Image Clicked!");
+          },
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Image.asset('assets/images/image.jpg',
+                  width: sliderValue,
+                  height: sliderValue,
+                ),
+                Divider(height: 20,),
+                Slider(
+                    value: sliderValue,
+                    min: 50,
+                    max: 300,
+                    divisions: 10,
+
+                    onChanged: (value) {
+                      setState(() {
+                        sliderValue = value;
+                      });
+                    },
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

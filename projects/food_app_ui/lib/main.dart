372532849be6c90940237fa9d0f0af7ac5723a99
Kibeyy <collinskibe6477@gmail.com>
2025-05-05 10:53:25 +0300

    Complete UI of hotel app

--- projects/food_app_ui/lib/main.dart
+++ projects/food_app_ui/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_app_ui/widgets/food_item.dart';
+import 'package:food_app_ui/widgets/hotel_item.dart';
 import 'package:food_app_ui/widgets/location_icon.dart';
+import 'package:food_app_ui/widgets/offer_widget.dart';
+import 'package:food_app_ui/widgets/see_all_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,11 +31,13 @@ class Home extends StatelessWidget {
         leading: IconButton(
             onPressed: (){},
             icon: LocationIcon()),
-        title: Text("Munchner Waisenhaus",
-          style: TextStyle(
-            color: Colors.lightBlueAccent,
-            fontWeight: FontWeight.bold,
+        title: FittedBox(
+          child: Text("Munchner Waisenhaus",
+            style: TextStyle(
+              color: Colors.lightBlueAccent,
+              fontWeight: FontWeight.bold,
 
+            ),
           ),
         ),
         actions: [
@@ -43,9 +48,87 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.shopping_cart))
         ],
       ),
-      body: Center(
-        child: FoodItem(),
-      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 15,left: 15,top: 20),
+          child: Column(
+
+            mainAxisSize: MainAxisSize.max,
+
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    FoodItem(imagePath: 'assets/images/alchohol.png', title: "Alchohol"),
+                    FoodItem(imagePath: 'assets/images/make up.png', title: "Make up"),
+                    FoodItem(imagePath: 'assets/images/pills.png', title: "Medicine"),
+                    FoodItem(imagePath: 'assets/images/sushi.png', title: "Food"),
+                    FoodItem(imagePath: 'assets/images/groceries.png', title: "Groceries")
+
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              OfferWidget(),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Dinner near you",style: TextStyle(fontWeight: FontWeight.bold),),
+                  SeeAllButton()
+
+                ],
+              ),
+              SizedBox(height: 10,),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    HotelItem(
+                        hotel_image_link: 'assets/images/kfc.png',
+                        hotel_name: "KFC",
+                        hotel_desc: "It's finger lickin' good"
+                    ),
+                    HotelItem(
+                        hotel_image_link: 'assets/images/dominosnbg.png',
+                        hotel_name: "Dominos",
+                        hotel_desc: "Get the door it's dominos"
+                    ),
+                    HotelItem(
+                        hotel_image_link: 'assets/images/creamyinn.png',
+                        hotel_name: "Creammy Inn",
+                        hotel_desc: "One scoop is never enough"
+                    )
+                  ],
+                ),
+              )
+
+
+            ],
+          ),
+        ),
+      )
+      ,
+      bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(
+                icon: Icon(Icons.location_city),
+                label: "Discovery"),
+            NavigationDestination(
+                icon: Icon(Icons.restaurant),
+                label: "Restaurant"),
+            NavigationDestination(
+                icon: Icon(Icons.store),
+                label: "Stores"),
+            NavigationDestination(
+                icon: Icon(Icons.search),
+                label: "Search"),
+            NavigationDestination(
+                icon: Icon(Icons.person),
+                label: "Profile")
+          ]),
     );
   }
 }
--- projects/food_app_ui/lib/widgets/food_item.dart
+++ projects/food_app_ui/lib/widgets/food_item.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
 class FoodItem extends StatelessWidget {
-  const FoodItem({super.key});
+  final String imagePath;
+  final String title;
+
+  const FoodItem({
+    super.key,
+    required this.imagePath,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100,height: 100,
-      child: Card(
-        shape: RoundedRectangleBorder(),
+      width: 100, height: 120,
+      child: Column(
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(25),
+            onTap: () {
 
+            },
+            splashColor: Colors.lightBlue,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20), // Adjust this value to control roundness
+              ),
+              child: SizedBox(
+                width: 80, // Fixed width for all images
+                height: 80, // Fixed height for all images
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover, // This makes the image cover the available space
+                ),
+              ),
+            ),
+          ),
+          //SizedBox(height: 5,),
+          Text(
+            title,
+            style: TextStyle(
+                fontWeight: FontWeight.bold
+            ),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis, // Handles long text
+          )
+        ],
       ),
     );
   }
